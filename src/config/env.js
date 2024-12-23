b1bb9863db6b8d2bafc0a44e345a66ac2104f607
Sergio Avalos <sergioai.si93@gmail.com>
2024-12-23 13:18:43 -0500

    config and version test

--- src/config/env.js
+++ src/config/env.js
@@ -1,28 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const {    
-    APP_NODE,
-    APP_ENV,
-    APP_HOST,
-    APP_PORT,
-    APP_NAME,
-    WRITE_PROJECT_ID,
-    WRITE_ENDPOINT,
-    WRITE_DATABASE,
-    WRITE_KEY
-} = process.env;
+const {
+  APP_ENV,
+  APP_HOST,
+  APP_PORT,
+  APP_NAME,
+  WRITE_PROJECT_ID,
+  WRITE_ENDPOINT,
+  WRITE_DATABASE,
+  WRITE_KEY,
+} = process.env
 
 export const APP = {
-    enviroment: APP_ENV,
-    port: APP_PORT ? Number(APP_PORT) : 8000,
-    host: APP_HOST,
-    name: APP_NAME
+  enviroment: APP_ENV,
+  port: APP_PORT ? Number(APP_PORT) : 8000,
+  host: APP_HOST,
+  name: APP_NAME,
 }
 
-
-
 export const WRITE = {
-    project: WRITE_PROJECT_ID|| 'default_project_id',
-    key: WRITE_KEY || 'default_api_key',
-    endpoint: WRITE_ENDPOINT|| 'http://localhost/v1',
-    database:WRITE_DATABASE || 'default_database_id'
+  project: WRITE_PROJECT_ID || 'default_project_id',
+  key: WRITE_KEY || 'default_api_key',
+  endpoint: WRITE_ENDPOINT || 'http://localhost/v1',
+  database: WRITE_DATABASE || 'default_database_id',
 }
